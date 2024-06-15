227f1eccb61d203cdbaabf0964d1d181dad4edd3
techysiddhant <sidjain8000@gmail.com>
2024-06-15 15:11:09 +0530

    pre typeorm setup

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -41,6 +41,26 @@ describe("POST /auth/register", () => {
                 expect.stringContaining("json"),
             );
         });
+        it("should persist the user in the database", async () => {
+            //TODO: Follow Three's AAA method
+            // Arrange Act Assert
+            //Arrange
+            const userData = {
+                firstName: "Siddhant",
+                lastName: "Jain",
+                email: "sidjain8000@gmail.com",
+                password: "siddhant",
+            };
+            //Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            //Assert
+            expect(response.headers["content-type"]).toEqual(
+                expect.stringContaining("json"),
+            );
+        });
     });
     describe("Fields are missing", () => {});
 });
