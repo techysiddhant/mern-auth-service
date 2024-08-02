49dcc5d562fe699d0cafad1923e48cb4879649f2
techysiddhant <sidjain8000@gmail.com>
2024-08-02 11:44:06 +0530

    complete tenant CRUD

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,4 +1,9 @@
-import express, { RequestHandler } from "express";
+import express, {
+    NextFunction,
+    Request,
+    RequestHandler,
+    Response,
+} from "express";
 import { Tenant } from "../entity/Tenant";
 import { AppDataSource } from "../config/data-source";
 import logger from "../config/logger";
@@ -7,6 +12,9 @@ import { canAccess } from "../middleware/canAccess";
 import { Roles } from "../constants";
 import { TenantService } from "../services/TenantService";
 import { TenantController } from "../controllers/TenantController";
+import tenantValidator from "../validators/tenant-validator";
+import listUsersValidator from "../validators/list-users-validator";
+import { CreateTenantRequest } from "../types";
 
 const router = express.Router();
 const tenantRepository = AppDataSource.getRepository(Tenant);
@@ -19,5 +27,33 @@ router.post(
     (async (req, res, next) => {
         await tenantController.create(req, res, next);
     }) as RequestHandler,
+);
+router.patch(
+    "/:id",
+    authenticate as RequestHandler,
+    canAccess([Roles.ADMIN]),
+    tenantValidator,
+    (req: CreateTenantRequest, res: Response, next: NextFunction) =>
+        tenantController.update(req, res, next) as unknown as RequestHandler,
+);
+router.get(
+    "/",
+    listUsersValidator,
+    (req: Request, res: Response, next: NextFunction) =>
+        tenantController.getAll(req, res, next) as unknown as RequestHandler,
+);
+router.get(
+    "/:id",
+    authenticate as RequestHandler,
+    canAccess([Roles.ADMIN]),
+    (req, res, next) =>
+        tenantController.getOne(req, res, next) as unknown as RequestHandler,
+);
+router.delete(
+    "/:id",
+    authenticate as RequestHandler,
+    canAccess([Roles.ADMIN]),
+    (req, res, next) =>
+        tenantController.destroy(req, res, next) as unknown as RequestHandler,
 );
 export default router;
--- src/types/index.ts
+++ src/types/index.ts
@@ -31,3 +31,32 @@ export interface ITenant {
 export interface CreateTenantRequest extends Request {
     body: ITenant;
 }
+
+export interface CreateUserRequest extends Request {
+    body: UserData;
+}
+
+export interface LimitedUserData {
+    firstName: string;
+    lastName: string;
+    role: string;
+    email: string;
+    tenantId: number;
+}
+
+export interface UpdateUserRequest extends Request {
+    body: LimitedUserData;
+}
+
+export interface UserQueryParams {
+    perPage: number;
+    currentPage: number;
+    q: string;
+    role: string;
+}
+
+export interface TenantQueryParams {
+    q: string;
+    perPage: number;
+    currentPage: number;
+}
